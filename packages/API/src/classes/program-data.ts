b5e765e9a868c3e43688d47202edad19322df707
dedeye <dedeye-wrk@yandex.ru>
2026-06-12 18:17:24 +0600

    fix: change fallback port from 4000 to 4001

--- packages/API/src/classes/program-data.ts
+++ packages/API/src/classes/program-data.ts
@@ -8,7 +8,7 @@ dotenv.config();
  * Объединённый класс данных приложения
  */
 export default class ProgramData {
-  public static readonly port = process.env.API_PORT ?? "4000";
+  public static readonly port = process.env.API_PORT ?? "4001";
   public static readonly uploadsDir = process.env.UPL_DIR ?? 'uploads';
   public static readonly dataDir = process.env.META_DIR ?? 'data';
   public static readonly dataFile = process.env.META_FILE ?? 'files.json';
